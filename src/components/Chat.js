26052fb14ffbb2d666761bac038d6efc3be96693
Adrian Mendez <adrianmndz328@gmail.com>
2021-03-10 01:28:21 -0600

    conversations are now rendering much better

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
+import { UserContext } from '../context'
 import ProfileIcon from './ProfileIcon'
 import '../styles/Chat.css'
 
 const Chat = props => {
 
     const { room } = props
+    const { user } = useContext(UserContext)
 
     const renderMessage = () => {
         if (room.messages.length === 0) {
             return 'No messages yet.'
         } else {
-            return room.messages[room.messages.length -1].content
+            let lastMessage = room.messages[room.messages.length - 1].content
+            if (lastMessage.length > 50) {
+                lastMessage = lastMessage.slice(0, 50)
+                lastMessage += '...'
+            }
+            return lastMessage
         }
     }
 
+    const fetchChatName = () => {
+        let username = ''
+        room.users.forEach(userInRoom => {
+            if (userInRoom.username !== user.username) {
+                username = userInRoom.username
+            }
+        });
+        return username
+    }
+
+    const chatName = fetchChatName()
+
     return (
         <Link to={`/room/${room.id}`} className="chat">
-            <ProfileIcon username={room.users[0].username}/>
+            <ProfileIcon username={chatName}/>
             <div className="content">
-                <div className="room-name">{room.name}</div>
+                <div className="room-name">{chatName}</div>
                 <div className="message">{renderMessage()}</div>
             </div>
         </Link>
