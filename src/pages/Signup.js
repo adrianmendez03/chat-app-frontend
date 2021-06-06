4b6efd27951cd4d3fc938f50577766b8b94fa36e
Adrian Mendez <adrianmndz328@gmail.com>
2021-06-05 23:45:00 -0500

    Added validation to Sign Up form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import "../styles/Signup.css"
 
@@ -11,12 +11,36 @@ const Signup = (props) => {
     password: "",
     username: "",
   }
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const validateInputs = (formVals) => {
+    setErrorMessage("")
+
+    // Really basic regex validation:
+    // Email validates basic structure -> _@_._
+    // Password validates password length greater than 8 and if it contains an uppercase and lowercase letter and a number
+    const emailValidation = new RegExp(/\S+@\S+\.\S+/)
+    const passwordValidation = new RegExp(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+    )
+
+    if (!emailValidation.test(formVals.email)) {
+      setErrorMessage("Enter a valid email")
+    } else if (!passwordValidation.test(formVals.password)) {
+      setErrorMessage(
+        "Password should be at least 8 characters long and contain a lowercase and uppercase letter, and a number."
+      )
+    } else {
+      handleSignup(formVals)
+    }
+  }
 
   return (
     <div id="signup" className="page">
       <div className="header">Signup</div>
       <div className="form-container">
-        <Form handleSubmit={handleSignup} form={emptyForm} />
+        {errorMessage.length > 0 && errorMessage}
+        <Form handleSubmit={validateInputs} form={emptyForm} />
         <Link to="/">Login</Link>
       </div>
     </div>
